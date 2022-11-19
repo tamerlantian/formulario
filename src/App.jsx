74f44cdd0bf7d69b4d42e5d7529c581f97b81806
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2022-11-18 19:46:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import RegisterForm from './components/RegisterForm'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <RegisterForm />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
